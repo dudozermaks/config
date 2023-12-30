f8f29e9c5d3aa7341d0f89b4c871d2f3b1840ed8
Maks <maks.razrushitel08@gmail.com>
2023-12-30 20:37:21 +0300

    add notebook support

---
+++ .config/custom_vars.sh
@@ -0,0 +1,11 @@
+PC_HOSTNAME="DM"
+NOTEBOOK_HOSTNAME="DMnotebook"
+HOSTNAME=$(cat /proc/sys/kernel/hostname)
+
+if [[ $HOSTNAME == $PC_HOSTNAME ]]; then
+	SHIT_FOLDER="$HOME/shit"
+	WALLPAPERS_FOLDER="$HOME/.config/wallpapers"
+else
+	SHIT_FOLDER="$HOME/shit"
+	WALLPAPERS_FOLDER="$HOME/.config/wallpapers"
+fi
