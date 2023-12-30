9a10049c3a5d3811376405b8513209108fdc4e16
Maks <maks.razrushitel08@gmail.com>
2023-12-30 20:08:16 +0300

    added screenlayout

---
+++ .screenlayout/main_pc.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --primary --mode 1920x1080 --pos 1680x0 --rotate normal --output DVI-D-0 --off --output HDMI-0 --off --output None-2-1 --off --output VGA-1-1 --mode 1680x1050 --pos 0x30 --rotate normal --output HDMI-1-1 --off
